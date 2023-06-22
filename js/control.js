85ebd3e66b45af4337b50a1f3a9090455b8dd082
TKuhner <trevorkuhner29@gmail.com>
2023-06-22 00:26:06 -0500

    added add role and add employee. need to update employee role, then work on bonus material

--- js/control.js
+++ js/control.js
@@ -8,63 +8,80 @@ const delay = ms => new Promise(res => setTimeout(res, ms));
 async function startPrompt(db) {
     await delay(100);
     const userInput = await inquirer.prompt([
-      {
-        name: 'userInput',
-        type: 'list',
-        message: 'What would you like to do?',
-        choices: inputList.inputList
-      }
+        {
+            name: 'userInput',
+            type: 'list',
+            message: 'What would you like to do?',
+            choices: inputList.inputList
+        }
     ]);
-  
+
     const handleUserInput = async (userInput) => {
-      switch (userInput) {
-        case 'View All Employees':
-          queryList.viewAllEmployees(db);
-          break;
-        case 'View All Departments':
-          queryList.viewAllDepartments(db);
-          break;
-        case 'View All Roles':
-          queryList.viewAllRoles(db);
-          break;
-        case 'Add Employee':
-          queryList.addEmployee(db);
-          break;
-        case 'Add Department':
-          const deptName = await addDepartment();
-          queryList.addDepartment(db, deptName);
-          break;
-        case 'Add Role':
-          const role = await addRole(db);
-          queryList.addRole(db,role);
-          break;
-        default:
-          console.log('Error');
-      }
-      startPrompt(db); // Recursive call
+        switch (userInput) {
+            case 'View All Employees':
+                queryList.viewAllEmployees(db);
+                break;
+            case 'View All Departments':
+                queryList.viewAllDepartments(db);
+                break;
+            case 'View All Roles':
+                queryList.viewAllRoles(db);
+                break;
+            case 'Add Employee':
+                const employee = await addEmployee(db);
+                queryList.addEmployee(db, employee);
+                break;
+            case 'Add Department':
+                const deptName = await addDepartment();
+                queryList.addDepartment(db, deptName);
+                break;
+            case 'Add Role':
+                const role = await addRole(db);
+                if (role === undefined) {
+                    console.log('Error: Role not added. Please try again.');
+                    break;
+                } else {
+                    queryList.addRole(db, role);
+                    break;
+                }
+            case 'Update Employee Role':
+            // queryList.updateEmployeeRole(db);
+            default:
+                console.log('Error');
+
+            // bonus cases
+            // Update employee managers.
+            // View employees by manager.
+            // View employees by department.
+            // Delete departments, roles, and employees.
+        }
+        startPrompt(db); // Recursive call
     };
-  
+
     await handleUserInput(userInput.userInput);
-  }
+}
 
 async function addDepartment() {
     try {
-      const response = await inquirer.prompt([
-        {
-          type: 'input',
-          message: 'What is the name of the department?',
-          name: 'departmentName'
-        }
-      ]);
-      return response.departmentName;
+        const response = await inquirer.prompt([
+            {
+                type: 'input',
+                message: 'What is the name of the department?',
+                name: 'departmentName'
+            }
+        ]);
+        return response.departmentName;
     } catch (error) {
-      console.error(error);
+        console.error(error);
     }
-  }
+}
 
 async function addRole(db) {
     try {
-        const depts = queryList.getAllDepartments(db);
+
+        const depts = await queryList.getAllDepartments(db);
+        const deptsIds = depts.map(dept => dept.id);
+        console.log("Please note, that the ID of the department you select must be a corresponding number to this table.")
         console.table(depts);
         const response = await inquirer.prompt([
             {
@@ -73,26 +90,87 @@ async function addRole(db) {
                 name: 'title'
             },
             {
-                type: 'input',
+                type: 'number',
                 message: 'What is the salary of the role?',
                 name: 'salary'
             },
             {
-                type: 'input',
-                message: 'What is the department id of the role?', 
+                type: 'number',
+                message: 'What is the department id of the role?',
                 name: 'department_id'
             }
         ]);
-        if (typeof response.title !== 'string'&& typeof response.salary !== 'number' && typeof response.department_id !== 'number') {
-            throw new Error('Please enter a valid input');
-            startPrompt();
+        if (isNaN(response.salary) || isNaN(response.department_id)) {
+            console.log('Please enter a valid input');
+            return;
         }
-        console.log(response);
-        return response;
+
+        if (!deptsIds.includes(response.department_id)) {
+            console.log('Please enter a valid department id');
+            return;
+        } else {
+            console.log('Role added!');
+            return response;
+        }
+
     } catch (error) {
         console.error(error);
     }
 }
+
+
+async function addEmployee(db) {
+    try {
+        const roles = await queryList.getAllRoles(db);
+        const rolesIds = roles.map(role => role.id);
+        const employees = await queryList.getAllEmployees(db);
+        const employeesIds = employees.map(employee => employee.id);
+        console.log("Please note, that the ID of the role you select must be a corresponding number to this table.")
+        console.table(roles);
+        console.table(employees);
+        const response = await inquirer.prompt([
+            {
+                type: 'input',
+                message: 'What is the first name of the employee?',
+                name: 'first_name'
+            },
+            {
+                type: 'input',
+                message: 'What is the last name of the employee?',
+                name: 'last_name'
+            },
+            {
+                type: 'number',
+                message: 'What is the role id of the employee?',
+                name: 'role_id'
+            },
+            {
+                type: 'number',
+                message: 'What is the manager id of the employee?',
+                name: 'manager_id'
+            }
+        ]);
+        if (isNaN(response.role_id)) {
+            console.log('Please enter a valid input');
+            return;
+        }
+        if (isNaN(response.manager_id) === true) {
+            response.manager_id = null;
+        }
+
+        if (!rolesIds.includes(response.role_id)) {
+            console.log('Please enter a valid role id');
+            return;
+        } else {
+            console.log('Employee added!');
+            return response;
+        }
+
+    } catch (error) {
+        console.error(error);
+    }
+}
+
 
 
 
--- js/queryList.js
+++ js/queryList.js
@@ -1,38 +1,76 @@
 
 const queryList = {
-    viewAllEmployees: function(db) {
-        db.query('SELECT * FROM employee', function(err, results) {
+    viewAllEmployees: function (db) {
+        db.query('SELECT * FROM employee', function (err, results) {
             console.table(results);
         });
     },
-    viewAllDepartments: function(db) {
-        db.query('SELECT * FROM department', function(err, results) {
+    getAllEmployees: function (db) {
+        return new Promise((resolve, reject) => {
+            db.query('SELECT * FROM employee', function (err, results) {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(results);
+                }
+            });
+        });
+    },
+    viewAllDepartments: function (db) {
+        db.query('SELECT * FROM department', function (err, results) {
             console.table(results);
         });
     },
-    getAllDepartments: function(db) {
-        db.query('SELECT * FROM department', function(err, results) {
-            return results;
+    getAllDepartments: function (db) {
+        return new Promise((resolve, reject) => {
+            db.query('SELECT * FROM department', function (err, results) {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(results);
+                }
+            });
         });
     },
-    viewAllRoles: function(db) {
-        db.query('SELECT * FROM role', function(err, results) {
+    viewAllRoles: function (db) {
+        db.query('SELECT * FROM role', function (err, results) {
             console.table(results);
         });
     },
-    addEmployee: function(db) {
-
-        db.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) ', function(err, results) {
-            console.table(results);
+    getAllRoles: function (db) {
+        return new Promise((resolve, reject) => {
+            db.query('SELECT * FROM role', function (err, results) {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(results);
+                }
+            });
         });
     },
-    addDepartment: function(db, departmentName) {
-        db.query('INSERT INTO department (department_name) VALUES (?)', [departmentName], function(err, results) {
+    addEmployee: function (db, employee) {
+        db.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)', [employee.first_name, employee.last_name, employee.role_id, employee.manager_id], function (err, results) {
+            if (err) throw err;
+        });
+        
+    },
+    addDepartment: function (db, departmentName) {
+        db.query('INSERT INTO department (department_name) VALUES (?)', [departmentName], function (err, results) {
             if (err) throw err;
         });
     },
-    addRole: function(db, role) {
-        // db.query('INSERT INTO role (title, salary, department_id) VALUES (?)', [role], function(err, results) {
+    addRole: function (db, role) {
+        db.query('INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)', [role.title, role.salary, role.department_id], function(err, results) {
+            if (err) {
+                throw err
+            } else {
+                console.log('Role added!');
+            }
+
+        });
+    },
+    updateEmployeeRole: function (db) {
+        // db.query('UPDATE employee SET role_id = ? WHERE id = ?', [role_id, id], function (err, results) {
         //     if (err) throw err;
         // });
     }
